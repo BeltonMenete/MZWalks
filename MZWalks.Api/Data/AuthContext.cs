43558e8dbb2ca68ebc02641e9adcaceba6206780
DevAlberto <beltonmenete@hotmail.com>
2025-07-05 02:17:07 +0200

    Add Db Roles to authDbContext

--- MZWalks.Api/Data/AuthContext.cs
+++ MZWalks.Api/Data/AuthContext.cs
@@ -1,4 +1,5 @@
 ﻿
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,4 +8,30 @@ namespace MZWalks.Api.Data;
 public class AuthContext(DbContextOptions<AuthContext> options): IdentityDbContext(options)
 {
     
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        const string readerRoleId = "01JZBW2J6W23RS9VF147NZAJ22";
+        const string writerRoleId = "01JZBWAXDKXG5G3TH5BKRWNFCG";
+
+        var roles = new List<IdentityRole>()
+        {
+            new IdentityRole()
+            {
+                Id = readerRoleId,
+                ConcurrencyStamp = readerRoleId,
+                Name = "Reader",
+                NormalizedName = "Reader".ToUpper()
+            },
+
+            new IdentityRole()
+            {
+                Id = writerRoleId,
+                ConcurrencyStamp = writerRoleId,
+                Name = "Writer",
+                NormalizedName = "Writer".ToUpper()
+            }
+        };
+        
+        modelBuilder.Entity<>()
 }
--- MZWalks.Api/Data/Context.cs
+++ MZWalks.Api/Data/Context.cs
@@ -9,32 +9,5 @@ public class Context(DbContextOptions<Context> options) : DbContext(options)
     public DbSet<Walk> Walks { get; set; }
     public DbSet<Region> Regions { get; set; }
     public DbSet<Difficulty> Difficulties { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-        const string readerRoleId = "01JZBW2J6W23RS9VF147NZAJ22";
-        const string writerRoleId = "01JZBWAXDKXG5G3TH5BKRWNFCG";
-        
-        var roles = new List<IdentityRole>()
-        {
-            new IdentityRole()
-            {
-                Id = readerRoleId,
-                ConcurrencyStamp = readerRoleId,
-                Name = "Reader",
-                NormalizedName = "Reader".ToUpper()
-                
-            },
-            
-            new IdentityRole()
-            {
-                Id = writerRoleId,
-                ConcurrencyStamp = writerRoleId,
-                Name = "Reader",
-                NormalizedName = "Reader".ToUpper()
-                
-            }
-        };
-    }
+    
 }
