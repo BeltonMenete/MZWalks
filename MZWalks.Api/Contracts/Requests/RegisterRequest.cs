621329e8192a86e3d0e4a00619d4b3dcb0ec6916
DevAlberto <beltonmenete@hotmail.com>
2025-07-05 04:29:25 +0200

    Add resgister action

--- MZWalks.Api/Contracts/Requests/RegisterRequest.cs
+++ MZWalks.Api/Contracts/Requests/RegisterRequest.cs
@@ -1,11 +1,10 @@
-﻿
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MZWalks.Api.Contracts.Requests;
 
 public class RegisterRequest
 {
-   [DataType(DataType.EmailAddress)]
-    public required string Username { get; set; }
-    public required string Password { get; set; }
+    [DataType(DataType.EmailAddress)] public required string UserName { get; set; }
+    [DataType(DataType.EmailAddress)] public required string Password { get; set; }
+    public required string[] Roles { get; set; }
 }
--- MZWalks.Api/Controllers/AuthController.cs
+++ MZWalks.Api/Controllers/AuthController.cs
@@ -1,15 +1,29 @@
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MZWalks.Api.Contracts.Requests;
 
 namespace MZWalks.Api.Controllers
 {
     [ApiController]
-    public class AuthController : ControllerBase
+    public class AuthController(UserManager<IdentityUser> userManager) : ControllerBase
     {
         [HttpPost(ApiEndpoints.Auth.Register)]
-        public async Task<IActionResult> Register([FromBody] RegisterRequest request) 
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
         {
+            var newIdentityUser = new IdentityUser()
+            {
+                UserName = request.UserName,
+                Email = request.UserName
+            };
+
+            var newIdentityResult = await userManager.CreateAsync(newIdentityUser, request.Password);
+
+            if (newIdentityResult.Succeeded)
+            {
+                // Add Roles To this user
+                
+            }
             return Ok();
         }
     }
