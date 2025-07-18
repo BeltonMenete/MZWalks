d6ba3fa2f7d3ee8ebda54081694e52a6534872a8
BeltonMenete <bymenete@gmail.com>
2025-07-18 17:57:15 +0200

    Add Images table to Context

--- MZWalks.Api/Data/Context.cs
+++ MZWalks.Api/Data/Context.cs
@@ -9,5 +9,4 @@ public class Context(DbContextOptions<Context> options) : DbContext(options)
     public DbSet<Walk> Walks { get; set; }
     public DbSet<Region> Regions { get; set; }
     public DbSet<Difficulty> Difficulties { get; set; }
-    
 }
