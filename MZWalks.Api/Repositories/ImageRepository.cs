dc81e8ab78d7bb386e93c3f44a37d01abbec8125
BeltonMenete <bymenete@gmail.com>
2025-07-19 06:03:25 +0200

    Add repository to image

---
+++ MZWalks.Api/Repositories/ImageRepository.cs
@@ -0,0 +1,13 @@
+﻿using MZWalks.Api.Data;
+
+namespace MZWalks.Api.Repositories;
+
+public class ImageRepository
+{
+    private readonly Context _context;
+
+    public ImageRepository(Context context)
+    {
+        _context = context;
+    }
+}
