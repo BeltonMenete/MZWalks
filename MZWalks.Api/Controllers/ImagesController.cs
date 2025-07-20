c4a4de33ac72f09990f69550b889aae5376e460a
BeltonMenete <bymenete@gmail.com>
2025-07-20 22:06:02 +0200

    Add to 'documentation' by using annotations for Scalar UI

--- MZWalks.Api/Controllers/ImagesController.cs
+++ MZWalks.Api/Controllers/ImagesController.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Mvc;
 using MZWalks.Api;
 using MZWalks.Api.Contracts.Requests;
-using MZWalks.Api.Mapping;
+using MZWalks.Api.Models.Domain;
 using MZWalks.Api.Repositories;
-using MZWalks.Api.Validators;
 
 [ApiController]
 public class ImagesController : ControllerBase
@@ -16,10 +16,12 @@ public class ImagesController : ControllerBase
     }
     
     [HttpPost(ApiEndpoints.Images.Upload)]
-    [EndpointSummary("Upload image")] 
+    [Consumes("multipart/form-data")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Image))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ValidationProblemDetails))]
+    [Produces("application/json")]
     public async Task<IActionResult> Upload([FromForm] CreateImageRequest request)
     {
-        // my custom file validation
         ValidateFileUpload(request);
 
         if (!ModelState.IsValid)
@@ -27,17 +29,14 @@ public class ImagesController : ControllerBase
             return BadRequest(ModelState);
         }
 
-        // Map DTO to Domain model, now correctly transferring the IFormFile
         var image = request.MapToImage();
 
-        // Repository to add image and save it locally
         var uploadedImage = await _imageRepository.Upload(image);
         return Ok(uploadedImage);
     }
 
     private void ValidateFileUpload(CreateImageRequest request)
     {
-        // Check if the file itself is null (should be caught by [Required] on DTO if present)
         if (request.File == null)
         {
             ModelState.AddModelError("File", "No file uploaded.");
@@ -46,7 +45,6 @@ public class ImagesController : ControllerBase
 
         var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
 
-        // Use request.File.FileName to get the actual uploaded file's extension
         var fileExtension = Path.GetExtension(request.File.FileName)?.ToLowerInvariant();
 
         if (!allowedExtensions.Contains(fileExtension))
@@ -54,7 +52,6 @@ public class ImagesController : ControllerBase
             ModelState.AddModelError("File", "Invalid file extension. Only .jpg, .jpeg, .png are allowed.");
         }
 
-        // 10 MB in bytes (10 * 1024 * 1024)
         if (request.File.Length > 10485760)
         {
             ModelState.AddModelError("File", "File size exceeds 10MB. Please upload a smaller size.");
